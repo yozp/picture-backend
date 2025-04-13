139de5295c3dade07f1567805d8b047139cb48a4
yozp <a1371361879@outlook.com>
2025-04-13 20:02:08 +0800

    Initial commit

---
+++ src/main/java/com/yzj/picturebackend/PictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yzj.picturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzj/picturebackend/PictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzj.picturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
